4d1d2f16de3399dd73994a56315587ca9941d57a
Madhan <120784875+madhanmaaz@users.noreply.github.com>
2025-05-22 17:28:44 +0530

    Create setup.py

---
+++ test/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="madhan",
+    version="0.0.1",
+    author="madhanmaaz",
+    description="install custom packages",
+    packages=find_packages(),
+    py_modules=["madhan"],
+    install_requires=[],
+)
