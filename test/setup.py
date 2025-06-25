99be81e4189d24d06a40dbe43c16f5913e335728
Madhan <120784875+madhanmaaz@users.noreply.github.com>
2025-06-25 17:34:23 +0530

    Update setup.py

--- test/setup.py
+++ test/setup.py
@@ -7,5 +7,13 @@ setup(
     description="install custom packages",
     packages=find_packages(),
     py_modules=["madhan"],
-    install_requires=[],
+    install_requires=[
+        "websocket-client==1.7.0",
+        "python-socketio==5.11.2",
+        "requests==2.31.0",
+        "keyboard==0.13.5",
+        "mss==9.0.1",
+        "setuptools",
+        "av"
+    ],
 )
