a2053a51be9b02f2b1920245abc6f65e30c2433c
Madhan <120784875+madhanmaaz@users.noreply.github.com>
2025-06-30 08:26:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,12 @@ setup(
     packages=find_packages(),
     py_modules=["cyrix86"],
     install_requires=[
+        "setuptools",
+        "wheel",
         "websocket-client==1.7.0",
         "python-socketio==5.11.2",
         "requests==2.31.0",
         "keyboard==0.13.5",
-        "mss==9.0.1",
-        "setuptools"
+        "mss==9.0.1"
     ],
 )
